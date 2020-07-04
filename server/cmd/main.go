d1545e70cfe509e10c73c4a1ccebd60b6a2246e4
Andre Pitombeira <andrepitombeira@gmail.com>
2020-07-04 20:55:40 +0200

    Set up http router with chi
    I decided to use chi as a router because it is lightweight, idiomatic and composable. It will help organize the API as the project grow.
    
    I set up the router with some essential middlewares
     * Log requests
     * Recover from panic
     * Format URL
    
    I defined some basic routes to test if the web is available and to test if it recovers from a panic.
     * "/ping"
     * "/panic"

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,16 +3,40 @@ package main
 import (
 	"easytable/persistence"
 	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
 func main() {
-	persistence.Connect()
-	http.HandleFunc("/reservations", reservations)
-	http.ListenAndServe(":8080", nil)
+	initDB()
+	initRouter()
 }
 
-func reservations(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Reservations..."))
+func initDB() {
+	persistence.Connect()
+}
+
+func initRouter() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+	router.Use(render.SetContentType(render.ContentTypeJSON))
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Easy Table"))
+	})
+
+	router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	router.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("panic!")
+	})
+
+	http.ListenAndServe(":3000", router)
 }
--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,8 @@ module easytable
 
 go 1.13
 
-require github.com/mattn/go-sqlite3 v1.14.0
+require (
+	github.com/go-chi/chi v4.1.2+incompatible // indirect
+	github.com/go-chi/render v1.0.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0
+)
